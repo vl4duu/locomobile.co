fe63a2b4058efb83eee7366b7bdc9d6104ba1435
Vlad <vlad.mutilica@gmail.com>
2024-11-09 22:39:53 +0000

    Rotate scene on mouse movement

--- src/components/ThreeGLBScene.vue
+++ src/components/ThreeGLBScene.vue
@@ -1,5 +1,5 @@
 <template>
-  <div ref="sceneContainer"></div>
+  <div ref="sceneContainer" class="scene-container"></div>
 </template>
 
 <script>
@@ -7,28 +7,30 @@ import * as THREE from 'three';
 import {GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader';
 import {getCurrentInstance} from 'vue'
 
-
 export default {
   name: 'ThreeGLBScene',
   data() {
     return {
       camera: null,
-      renderer: null,
+      mouse: new THREE.Vector2(),
+      rotationTarget: new THREE.Vector2(),
+      windowHalfX: window.innerWidth / 2,
+      windowHalfY: window.innerHeight / 2,
+      instance: getCurrentInstance(),
       elements: [],
-      instance: null,
-      clock: null,
-      mixer: null
+      clock: new THREE.Clock(),
+      mixer: null,
+      renderer: null,
     }
   },
   async mounted() {
-    this.instance = getCurrentInstance();
     this.initThree();
-    window.addEventListener('resize', this.onWindowResize);
+    this.addEventListeners()
     this.loadScene();
     this.animate();
   },
   beforeDestroy() {
-    window.removeEventListener('resize', this.onWindowResize);
+    this.removeEventListeners()
     if (this.renderer) {
       this.renderer.dispose();
     }
@@ -37,15 +39,10 @@ export default {
     initThree() {
       const {proxy} = this.instance;
       proxy.$scene = new THREE.Scene();
-      this.clock = new THREE.Clock();
+
       proxy.$elements = [];
 
       // Camera
-      const aspect = this.$refs.sceneContainer.clientWidth / this.$refs.sceneContainer.clientHeight;
-      this.camera = new THREE.PerspectiveCamera(110, aspect, 0.1, 10000);
-      this.camera.position.set(-13.021, 0.660, 11.823); // Adjust camera position
-      this.camera.rotation.x = 2500
-      this.camera.rotation.y = -150
       //Renderer does the job of rendering the graphics
       this.renderer = new THREE.WebGLRenderer({antialias: true});
       this.renderer.setSize(this.$refs.sceneContainer.clientWidth, this.$refs.sceneContainer.clientHeight);
@@ -71,14 +68,20 @@ export default {
       loader.load(
           '/spinning-disc.gltf', // Adjust the path as needed
           (gltf) => {
-            // this.instance.proxy.$scene.add(gltf.scene)
             gltf.scene.traverse((obj) => {
               this.instance.proxy.$elements.push(obj)
             })
-            this.instance.proxy.$elements.forEach((el) => {
-              this.instance.proxy.$scene.add(el)
-            })
 
+            for (let i = 1; i < this.instance.proxy.$elements.length; i++) {
+              if (this.instance.proxy.$elements[i].name !== 'front_view_camera') {
+                this.instance.proxy.$elements[0].add(this.instance.proxy.$elements[i]);
+              }
+            }
+            this.instance.proxy.$scene.add(this.instance.proxy.$elements[0])
+
+            const camera = this.instance.proxy.$elements.find((el) => el.name === 'front_view_camera');
+            this.instance.proxy.$scene.add(camera);
+            console.log(this.instance.proxy.$scene)
             this.setupScene(gltf);
 
           },
@@ -93,55 +96,95 @@ export default {
     },
     setupScene: function (gltf) {
       this.setupCamera();
-      this.setupMixer(gltf);
+      this.setupAnimationMixer(gltf);
       this.setupLight();
     },
-    setupMixer: function (gltf) {
+    setupAnimationMixer: function (gltf) {
       if (gltf.animations.length > 0) {
         this.mixer = new THREE.AnimationMixer(this.instance.proxy.$scene);
         this.mixer.clipAction(gltf.animations[0]).play();
       }
     },
     setupCamera: function () {
-      let sceneCamera = this.instance.proxy.$scene.children.find((item) => item.name === 'front_view_camera')
+      const sceneCamera = this.instance.proxy.$scene.children[1]
+      sceneCamera.aspect = this.$refs.sceneContainer.clientWidth / this.$refs.sceneContainer.clientHeight;
       this.camera = sceneCamera
     },
     setupLight: function () {
-      let sceneLight = this.instance.proxy.$scene.children.find((item) => item.name === 'light')
-      sceneLight.intensity = 160
+      let sceneLight = this.instance.proxy.$scene.children[0].children.find((item) => item.name === 'light')
+      sceneLight.intensity = 130
       sceneLight.castShadow = true
     },
     setupDisc: function () {
     },
-    setup: function () {
+    setupBackDrop: function () {
     },
     animate() {
       this.updateCameraAspect();
       requestAnimationFrame(this.animate);
-      this.renderer.render(this.instance.proxy.$scene, this.camera);
+
+      if (this.camera) {
+        this.renderer.render(this.instance.proxy.$scene, this.camera);
+      }
+
       if (this.mixer) {
         this.mixer.update(this.clock.getDelta());
       }
+      this.handleMouseMove()
+
+    },
+    handleMouseMove: function () {
+      const {mouse, rotationTarget} = this
+      let targetX = mouse.x * .0001
+      let targetY = mouse.y * .0001
+
+      let scene = this.instance.proxy.$scene.children[0]
+
+      if (scene) {
+        rotationTarget.x += targetY - scene.rotation.x
+        rotationTarget.y += targetX - scene.rotation.y
+        scene.rotation.set(rotationTarget.x, rotationTarget.y, scene.rotation.z)
+      }
     },
     onWindowResize() {
-      console.log(1)
       this.updateCameraAspect();
     },
+    onMouseMove(event) {
+      const {mouse, windowHalfX, windowHalfY} = this
+      mouse.x = (event.clientX - windowHalfX)
+      mouse.y = (event.clientY - windowHalfY)
+    },
+    addEventListeners: function () {
+      window.addEventListener('resize', this.onWindowResize);
+      window.addEventListener('mousemove', this.onMouseMove);
+    },
+    removeEventListeners: function () {
+      window.removeEventListener('resize', this.onWindowResize);
+      window.removeEventListener('mousemove', this.onMouseMove);
+    },
     updateCameraAspect() {
+      if (!this.camera) return
+
       const width = window.innerWidth;
       const height = window.innerHeight;
+
       this.camera.aspect = width / height;
       this.camera.updateProjectionMatrix();
+
       this.renderer.setSize(width, height);
+
     }
-  }
+  },
+
 }
 </script>
 
 <style>
 .scene-container {
-  width: 100%;
-  height: 100vh;
-  overflow: hidden;
+  overflow: clip;
+  position: absolute;
+  top: 0;
+  left: 0;
+  outline: none;
 }
 </style>
