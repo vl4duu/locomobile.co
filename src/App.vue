885b82b567789b25f73d8570f9c1d886b18be836
Vlad <vlad.mutilica@gmail.com>
2024-12-08 21:20:29 +0000

    Add spinning blocks loading animation

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,15 @@
 import ThreeGLBScene from "@/components/ThreeGLBScene.vue";
 import NavBar from "@/components/NavBar.vue";
 import {darkTheme} from 'naive-ui'
+import LoadingAnimation from "@/components/LoadingAnimation.vue";
 
 </script>
 
 <template>
   <main>
     <NavBar/>
-    <ThreeGLBScene/>
+<!--    <ThreeGLBScene/>-->
+    <LoadingAnimation/>
   </main>
 </template>
 
---
+++ src/components/LoadingAnimation.vue
@@ -0,0 +1,39 @@
+<!--https://codepen.io/aaroniker/pen/ZmOMJp-->
+<script setup>
+</script>
+
+<template>
+  <div class="boxes">
+    <div class="box">
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+    <div class="box">
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+    <div class="box">
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+    <div class="box">
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  </div>
+
+  <!-- dribbble -->
+  <a class="dribbble" href="https://dribbble.com/shots/5533600-Loading-boxes" target="_blank"><img
+      src="https://cdn.dribbble.com/assets/dribbble-ball-mark-2bd45f09c2fb58dbbfb44766d5d1d07c5a12972d602ef8b32204d28fa3dda554.svg"
+      alt=""></a>
+</template>
+
+<style src="../assets/LoadingScreen/loadingAnimation.css"></style>
